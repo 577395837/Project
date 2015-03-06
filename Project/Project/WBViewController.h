a03c58a5c6105ca03bce50571fff767326ddb1bb
wxb <577395837@qq.com>
2015-03-06 09:51:25 +0800

    WBViewController

---
+++ Project/Project/WBViewController.h
@@ -0,0 +1,13 @@
+//
+//  WBViewController.h
+//  Project
+//
+//  Created by wxb on 15-3-6.
+//  Copyright (c) 2015年 bei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WBViewController : UIViewController
+
+@end
---
+++ Project/Project/WBViewController.m
@@ -0,0 +1,30 @@
+//
+//  WBViewController.m
+//  Project
+//
+//  Created by wxb on 15-3-6.
+//  Copyright (c) 2015年 bei. All rights reserved.
+//
+
+#import "WBViewController.h"
+
+@interface WBViewController ()
+
+@end
+
+@implementation WBViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    NSLog(@"WBViewController");
+
+
+}
+
+
+
+
+
+@end
